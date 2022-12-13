2528608a16fabfb1536c1d1a885457e563d7a675
Ivan Beshkiltsev <ivanbeshka2002@gmail.com>
2022-12-13 18:21:07 +0500

    complete hw 4

---
+++ skillbox/hw_4/bookshop/MyBookShopApp/src/main/java/com/example/MyBookShopApp/data/BookService.java
@@ -0,0 +1,40 @@
+package com.example.MyBookShopApp.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BookService {
+
+    private final JdbcTemplate jdbcTemplate;
+    private static final int NEW_BOOKS_COUNT = 10;
+
+    @Autowired
+    public BookService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Book> getBooksData() {
+        List<Book> books = jdbcTemplate.query("SELECT * FROM books", (ResultSet rs, int rowNum) -> {
+            Book book = new Book();
+            book.setId(rs.getInt("id"));
+            book.setAuthor(rs.getString("author"));
+            book.setTitle(rs.getString("title"));
+            book.setPriceOld(rs.getString("priceOld"));
+            book.setPrice(rs.getString("price"));
+            return book;
+        });
+        return new ArrayList<>(books);
+    }
+
+    public List<Book> getNewBooks() {
+        List<Book> newBooks = new ArrayList<>(getBooksData());
+        newBooks = newBooks.subList(0, Math.min(newBooks.size(), NEW_BOOKS_COUNT));
+        return newBooks;
+    }
+}
